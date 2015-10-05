e567d7b306bc2043cd6cdf7d42c6fe9da746f12c
Alex Jironkin <Aleksey.Jironkin@phe.gov.uk>
2015-10-05 10:28:55 +0100

    Extracted creation of auxiliary files for variant callers to separate method and required to be implemented as aprt of VariantCaller implementation.

--- phe/variant/GATKVariantCaller.py
+++ phe/variant/GATKVariantCaller.py
@@ -3,23 +3,23 @@ Created on 22 Sep 2015
 
 @author: alex
 '''
+import logging
 import os
 
 from phe.variant import VariantCaller
 
 
 class GATKVariantCaller(VariantCaller):
-    '''
-    classdocs
-    '''
+    """Implemetation of the Broad institute's variant caller."""
 
     name = "gatk"
+    """Plain text name of the variant caller."""
+
     _default_options = "-nt 1"
+    """Default options for the variant caller."""
 
     def __init__(self, cmd_options=None):
-        '''
-        Constructor
-        '''
+        """Constructor"""
         if cmd_options is None:
             cmd_options = self._default_options
 
@@ -30,26 +30,71 @@ class GATKVariantCaller(VariantCaller):
         ref = kwargs.get("ref")
         bam = kwargs.get("bam")
 
-        if kwargs.get("out_dir") is None:
-            kwargs["out_dir"] = "."
+        if kwargs.get("vcf_file") is None:
+            kwargs["vcf_file"] = "variants.vcf"
 
-        variant_dir = os.path.abspath(kwargs.get("out_dir"))
-        ref_name, _ = os.path.splitext(ref)
         opts = {"ref": ref,
-                "ref_name": ref_name,
                 "bam": bam,
-                "picard_tools_path": os.path.join(os.environ["PICARD_TOOLS_PATH"], "CreateSequenceDictionary.jar"),
                 "ploidy": 2,
                 "glm": "BOTH",
-                "all_variants_file": os.path.join(variant_dir, "variants.vcf"),
+                "all_variants_file": kwargs.get("vcf_file"),
                 "extra_cmd_options": self.cmd_options}
 
-        os.system("samtools faidx %(ref)s" % opts)
+        if not self.create_aux_files(ref):
+            logging.warn("Auxiliary files were not created.")
+            return False
 
-        os.system("java -jar %(picard_tools_path)s R=%(ref)s O=%(ref_name)s.dict" % opts)
-
-        #  call variants
+        # Call variants
         # FIXME: Sample ploidy = 2?
-        os.system("java -XX:+UseSerialGC -jar $GATK_JAR -T UnifiedGenotyper -R  %(ref)s --sample_ploidy %(ploidy)s --genotype_likelihoods_model %(glm)s -rf BadCigar -out_mode EMIT_ALL_SITES -I %(bam)s -o %(all_variants_file)s %(extra_cmd_options)s" % opts)
+        success = os.system("java -XX:+UseSerialGC -jar $GATK_JAR -T UnifiedGenotyper -R  %(ref)s --sample_ploidy %(ploidy)s --genotype_likelihoods_model %(glm)s -rf BadCigar -out_mode EMIT_ALL_SITES -I %(bam)s -o %(all_variants_file)s %(extra_cmd_options)s" % opts)
 
-        return opts["all_variants_file"]
+        if success != 0:
+            logging.warn("Calling variants returned non-zero exit status.")
+            return False
+
+        return True
+
+    def create_aux_files(self, ref):
+        """Create auxiliary files needed for this variant.
+
+        Tools needed: samtools and picard tools. Picard tools is a Java
+        library that can be defined using environment variable: PICARD_JAR
+        specifying path to picard.jar or PICARD_TOOLS_PATH specifying path
+        to the directory where separate jars are (older version before jars
+        were merged into a single picard.jar).
+        Parameters:
+        -----------
+        ref: str
+            Path to the reference file.
+        
+        Returns:
+        --------
+        bool:
+            True if auxiliary files were created, False otherwise.
+        """
+
+        ref_name, _ = os.path.splitext(ref)
+
+        success = os.system("samtools faidx %s" % ref)
+
+        if success != 0:
+            logging.warn("Fasta index could not be created.")
+            return False
+
+        d = {"ref": ref, "ref_name": ref_name}
+
+        if os.environ["PICARD_TOOLS_PATH"]:
+            d["picard_tools_path"] = os.path.join(os.environ["PICARD_TOOLS_PATH"], "CreateSequenceDictionary.jar")
+        elif os.environ["PICARD_JAR"]:
+            # This is used in newer version of PICARD tool where multiple
+            #    jars were merged into a single jar file.
+            d["picard_tools_path"] = "%s %s" % (os.environ["PICARD_JAR"], "CreateSequenceDictionary.jar")
+        else:
+            logging.error("Picard tools are not present in the path.")
+            return False
+
+        success = os.system("java -jar %(picard_tools_path)s R=%(ref)s O=%(ref_name)s.dict" % d)
+
+        if success != 0:
+            logging.warn("Dictionary for the %s reference could not be created", ref)
+            return False
--- phe/variant/__init__.py
+++ phe/variant/__init__.py
@@ -1,3 +1,4 @@
+"""Classes and methods to work with variants and such."""
 import abc
 import pickle
 
@@ -8,6 +9,8 @@ from vcf.parser import _Filter
 from phe.variant_filters import make_filters
 
 class VCFTemplate(object):
+    """This is a small hack class for the Template used in generating
+    VCF file."""
 
     def __init__(self, vcf_reader):
         self.infos = vcf_reader.infos
@@ -20,18 +23,32 @@ class VCFTemplate(object):
         self.samples = vcf_reader.samples
 
 class VariantSet(object):
+    """A convenient representation of set of variants.
+    TODO: Implement iterator and generator for the variant set.
+    """
 
     def __init__(self, vcf_in, filters=None):
-
+        """Constructor of variant set.
+        
+        Parameters:
+        -----------
+        vcf_in: str
+            Path to the VCF file for loading information.
+        filters: dict, optional
+            Dictionary of the filter, threshold key value pairs.
+        """
         self.vcf_in = vcf_in
 
         self.out_template = None
 
-        self.filters = make_filters(config=filters)
+        self.filters = []
+        if filters is not None:
+            self.filters = make_filters(config=filters)
+
         self.variants = []
 
-    def make_variant_set(self, keep_only_snps=True):
-        """WIP This should go somewhere else."""
+    def filter_variants(self, keep_only_snps=True):
+        """Create a variant """
 
         # Create a reader class from input VCF.
         reader = vcf.Reader(filename=self.vcf_in)
@@ -56,20 +73,9 @@ class VariantSet(object):
                 if result == None:
                     continue
 
-                # save some work by skipping the rest of the code
-    #             if drop_filtered:
-    #                 output_record = False
-    #                 break
-
                 # If we got this far, then record is filtered OUT.
                 record.add_filter(record_filter.filter_name())
-    #             if short_circuit: break
 
-    #         if output_record:
-    #             # use PASS only if other filter names appear in the FILTER column
-    #             # FIXME: is this good idea?
-    #             if record.FILTER is None or record.FILTER = 'PASS':
-    #             output.write_record(record)
             # After applying all filters, check if FILTER is None.
             # If it is, then record PASSED all filters.
             if record.FILTER is None:
@@ -80,14 +86,29 @@ class VariantSet(object):
             else:
                 self.variants.append(record)
 
-
         self.out_template = VCFTemplate(reader)
 
         return [ variant for variant in self.variants if variant.FILTER == "PASS"]
 
 
     def write_variants(self, vcf_out, only_snps=False, only_good=False):
-
+        """Write variants to a VCF file.
+        
+        Parameters:
+        -----------
+        vcf_out: str
+            Path to the file where VCF data is written.
+        only_snps: bool, optional
+            True is *only* SNP are to be written to the output (default: False).
+        only_good: bool, optional
+            True if only those records that PASS all filters should be written
+            (default: False).
+        
+        Returns:
+        int:
+            Number of records written.
+        """
+        written_variants = 0
         with open(vcf_out, "w") as out_vcf:
             writer = vcf.Writer(out_vcf, self.out_template)
             for record in self.variants:
@@ -95,32 +116,86 @@ class VariantSet(object):
                 if only_snps and not record.is_snp:
                     continue
 
-                if only_good and record.FILTER != "PASS":
+                if only_good and record.FILTER != "PASS" or record.FILTER is None:
                     continue
 
                 writer.write_record(record)
+                written_variants += 1
+
+        return written_variants
 
     def _write_bad_variants(self, vcf_out):
+        """**PRIVATE:** Write only those records that **haven't** passed."""
+        written_variants = 0
         with open(vcf_out, "w") as out_vcf:
             writer = vcf.Writer(out_vcf, self.out_template)
             for record in self.variants:
-                if record.FILTER != "PASS":
+                if record.FILTER != "PASS" and record.FILTER is not None:
                     writer.write_record(record)
+                    written_variants += 1
+        return written_variants
 
     def serialise(self, out_file):
+        """Save the data in this class to a file for future use/reload.
+        
+        Parameters:
+        -----------
+        out_file: str
+            path to file where the data should be written to.
+            
+        Returns:
+        --------
+        int:
+            Number of variants written.
+        """
+        written_variants = 0
         with open(out_file, "w") as out_vcf:
             writer = vcf.Writer(out_vcf, self.out_template)
             for record in self.variants:
                 writer.write_record(record)
+                written_variants += 1
+
+        return written_variants
 
 
 class VariantCaller(object):
+    """Abstract class used for access to the implemented variant callers."""
 
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, cmd_options=None):
+        """Constructor for variant caller.
+        
+        Parameters:
+        -----------
+        cmd_options: str, optional
+            Command options to pass to the variant command.
+        """
         self.cmd_options = cmd_options
 
     @abc.abstractmethod
     def make_vcf(self, *args, **kwargs):
+        """Create a VCF from **BAM** file.
+        
+        Parameters:
+        -----------
+        ref: str
+            Path to the reference file.
+        bam: str
+            Path to the indexed **BAM** file for calling variants.
+        vcf_file: str
+            path to the VCF file where data will be written to.
+            
+        Returns:
+        --------
+        bool:
+            True if variant calling was successful, False otherwise.
+        """
         raise NotImplementedError("make_vcf is not implemented yet.")
+
+    @abc.abstractmethod
+    def create_aux_files(self, ref):
+        """Create needed (if any) auxiliary files.
+        These files are required for proper functioning of the variant caller.
+        """
+        raise NotImplementedError("create_aux_files is not implemeted.")
