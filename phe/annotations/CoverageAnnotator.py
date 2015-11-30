0389b5f129ca40f5ccc38adfe0dab6446e291f76
Alex Jironkin <Aleksey.Jironkin@phe.gov.uk>
2015-11-30 15:33:14 +0000

    Rejigged the math import

--- phe/annotations/CoverageAnnotator.py
+++ phe/annotations/CoverageAnnotator.py
@@ -4,7 +4,7 @@ Created on 10 Nov 2015
 @author: alex
 '''
 from collections import OrderedDict
-from math import sqrt
+import math
 
 import vcf
 
@@ -39,7 +39,7 @@ class CoverageAnnotator(Annotator):
 
         self.mean = sum(dp) * 1.0 / len(dp)
         mean_sqr = sum([i ** 2 for i in dp]) * 1.0 / len(dp)
-        self.dev = sqrt(mean_sqr - self.mean ** 2)
+        self.dev = math.sqrt(mean_sqr - self.mean ** 2)
 
     def get_meta_values(self):
         return OrderedDict({"mean": "%.2f" % self.mean, "dev": "%.2f" % self.dev})
