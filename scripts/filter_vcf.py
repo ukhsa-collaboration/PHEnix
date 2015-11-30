38a0c775322d0391cab1efc4ff4e00ce92e45762
Alex Jironkin <Aleksey.Jironkin@phe.gov.uk>
2015-11-30 15:43:31 +0000

    Option to write only SNPs that PASSED the filters.

--- scripts/filter_vcf.py
+++ scripts/filter_vcf.py
@@ -15,8 +15,9 @@ def get_args():
     args = argparse.ArgumentParser()
 
     args.add_argument("--vcf", "-v", required=True, help="VCF file to (re)filter.")
-    args.add_argument("--filters", "-f", required=True, help="Filter(s) to apply as key:threshold pairs, separated by comma.")
+    args.add_argument("--filters", "-f", help="Filter(s) to apply as key:threshold pairs, separated by comma.")
     args.add_argument("--output", "-o", required=True, help="Location for filtered VCF to be written.")
+    args.add_argument("--only-good", action="store_true", default=False, help="Write only variants that PASS all filters (default all variants are written).")
 
     return args.parse_args()
 
@@ -26,9 +27,12 @@ def main():
 
     var_set = VariantSet(args.vcf, filters=args.filters)
 
-    var_set.filter_variants()
+    if args.filters:
+        var_set.filter_variants()
 
-    var_set.serialise(args.output)
+    var_set.write_variants(args.output, only_good=args.only_good)
+
+
 
 if __name__ == '__main__':
     exit(main())
