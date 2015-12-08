1add5e4e817aae7bedd5a02fcbdf41a18f3d1161
Alex Jironkin <Aleksey.Jironkin@phe.gov.uk>
2015-12-08 14:58:25 +0000

    Filter now checks for *config* or *filters* being specified.

--- scripts/filter_vcf.py
+++ scripts/filter_vcf.py
@@ -6,6 +6,8 @@ Created on 6 Oct 2015
 @author: alex
 '''
 import argparse
+import logging
+import yaml
 
 from phe.variant import VariantSet
 
@@ -15,15 +17,38 @@ def get_args():
     args = argparse.ArgumentParser()
 
     args.add_argument("--vcf", "-v", required=True, help="VCF file to (re)filter.")
-    args.add_argument("--filters", "-f", help="Filter(s) to apply as key:threshold pairs, separated by comma.")
+
+    group = args.add_mutually_exclusive_group()
+
+    group.add_argument("--filters", "-f", help="Filter(s) to apply as key:threshold pairs, separated by comma.")
+    group.add_argument("--config", "-c", help="Config with filters in YAML format. E.g.filters:-key:value")
+
     args.add_argument("--output", "-o", required=True, help="Location for filtered VCF to be written.")
+
     args.add_argument("--only-good", action="store_true", default=False, help="Write only variants that PASS all filters (default all variants are written).")
+
+    args.add_argument("--debug", action="store_true", default=False, help="Make output more verbose.")
 
     return args.parse_args()
 
+def load_config(config_path):
+    with open(config_path) as fp:
+        config = yaml.load(fp)
+
+    return config.get("filters", {})
 
 def main():
     args = get_args()
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s",
+                            level=log_level)
+
+    if args.config is not None:
+        args.filters = load_config(args.config)
+    elif args.filters is None:
+        logging.error("Either --config or --filters needs to be specified.")
+        return 1
 
     var_set = VariantSet(args.vcf, filters=args.filters)
 
