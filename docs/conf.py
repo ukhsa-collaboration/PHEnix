e303eed8b40fdb46398344d8c2975ab46b2ab0c1
Alex Jironkin <Aleksey.Jironkin@phe.gov.uk>
2016-04-07 15:02:48 +0100

    Changed theme to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -115,20 +116,18 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'phe'
+# html_theme = 'phe'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'bootstrap'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "edit_link": False,
-    "sidebar": "right",
-    "scipy_org_logo": "true",
-    #"rootlinks": [("http://158.119.147.111/docs", "Bioinformatics Docs")]
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_theme"]
+html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
