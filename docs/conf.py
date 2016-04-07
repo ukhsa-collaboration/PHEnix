67ae1c7883564d17ff271cfd104a0dde1a2b602d
Richard Myers <richard.myers@phe.gov.uk>
2016-04-07 15:32:00 +0100

    modified conf.py to remove the requirement for a theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@
 import os
 import sys
 
-import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
